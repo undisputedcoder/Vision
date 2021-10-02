53524e91475834c9c51bf9fd0bdd84bdb5c3e827
Trykelvinator <87376656+Trykelvinator@users.noreply.github.com>
2021-10-02 14:51:47 +0800

    Splash screen for Vision.
    Splash screen for Vision, called by main and calls LoginPage() from login.dart.

---
+++ lib/Screens/splash-screen.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:apple/Screens/login.dart';
+import 'package:flutter/cupertino.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 2),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => LoginPage())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: CupertinoColors.white,
+      child: Transform.scale(
+        scale: 0.4,
+        child: const Image(
+          image: AssetImage(
+            'Assets/splash-screen.png',
+          ),
+        ),
+      ),
+    );
+  }
+}
